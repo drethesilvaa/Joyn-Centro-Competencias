f7201f41512e2665110af9cc575901dc95008f6b
drethesilva <andressilva03@gmail.com>
2025-06-26 16:39:07 +0100

    centros tina cms file

--- frontend/tina/config.ts
+++ frontend/tina/config.ts
@@ -173,6 +173,335 @@ export default defineConfig({
           },
         ],
       },
+      {
+        name: "centrosDeCompetencia",
+        label: "Centros de Competencia",
+        path: "content/centrosDeCompetencia",
+        format: "json",
+        ui: {
+          allowedActions: { create: false, delete: false }, // optional, disable creating/deleting
+        },
+        fields: [
+          {
+            type: "object",
+            name: "header",
+            label: "Header",
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+                required: true,
+              },
+              {
+                type: "string",
+                name: "description",
+                label: "Description",
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                type: "string",
+                name: "videoUrl",
+                label: "Video URL",
+              },
+              {
+                type: "image",
+                name: "imageToSwapForVideo",
+                label: "Image to Swap for Video",
+              },
+            ],
+          },
+          {
+            type: "object",
+            name: "objectives",
+            label: "Objetivos",
+            fields: [
+              {
+                type: "object",
+                name: "topics",
+                label: "Tópicos",
+                list: true,
+                ui: {
+                  itemProps: (item) => {
+                    return { label: item?.description || "" };
+                  },
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "iconName",
+                    label: "Icon Name",
+                  },
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Description",
+                    ui: {
+                      component: "textarea",
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: "object",
+            name: "centrosDeCompetencia",
+            label: "Centros de Competencia",
+            ui: {
+              itemProps: (item) => {
+                return { label: item?.title || "" };
+              },
+            },
+            list: true,
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+                required: true,
+              },
+              {
+                type: "string",
+                name: "description",
+                label: "Description",
+                ui: {
+                  component: "textarea",
+                },
+              },
+              // Charts Array
+              {
+                type: "object",
+                name: "charts",
+                label: "Charts",
+                list: true,
+                ui: {
+                  itemProps: (item) => {
+                    return { label: item?.title || "New Chart" };
+                  },
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "title",
+                    label: "Title",
+                  },
+                  {
+                    type: "string",
+                    name: "chartType",
+                    label: "Chart Type",
+                    required: true,
+                    options: [
+                      { value: "bar", label: "Bar Chart" },
+                      { value: "line", label: "Line Chart" },
+                      { value: "pie", label: "Pie Chart" },
+                    ],
+                  },
+                  {
+                    type: "object",
+                    name: "values",
+                    label: "Values",
+                    list: true,
+                    ui: {
+                      itemProps: (item) => {
+                        return { label: item?.name || "" };
+                      },
+                    },
+                    fields: [
+                      {
+                        type: "string",
+                        name: "name",
+                        label: "Name",
+                        required: true,
+                      },
+                      {
+                        type: "number",
+                        name: "value",
+                        label: "Value",
+                        required: true,
+                      },
+                    ],
+                  },
+                ],
+              },
+              // Learning Array
+              {
+                type: "object",
+                name: "learning",
+                label: "Learning",
+                list: true,
+                ui: {
+                  itemProps: (item) => {
+                    return { label: item?.title || "" };
+                  },
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "title",
+                    label: "Title",
+                    required: true,
+                  },
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Description",
+                    ui: {
+                      component: "textarea",
+                    },
+                  },
+                  // Topics Array
+                  {
+                    type: "object",
+                    name: "topics",
+                    label: "Topics",
+                    list: true,
+                    ui: {
+                      itemProps: (item) => {
+                        return { label: item?.topicTitle || "" };
+                      },
+                    },
+                    fields: [
+                      {
+                        type: "string",
+                        name: "topicTitle",
+                        label: "Topic Title",
+                        required: true,
+                      },
+                      {
+                        type: "object",
+                        name: "topicDetails",
+                        label: "Topic Details",
+                        list: true,
+                        ui: {
+                          itemProps: (item) => {
+                            return { label: item?.description || "" };
+                          },
+                        },
+                        fields: [
+                          {
+                            type: "string",
+                            name: "iconName",
+                            label: "Icon Name",
+                            required: true,
+                          },
+                          {
+                            type: "string",
+                            name: "description",
+                            label: "Description",
+                            ui: {
+                              component: "textarea",
+                            },
+                          },
+                          {
+                            type: "string",
+                            name: "url",
+                            label: "URL",
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+              // Feedback Object
+              {
+                type: "object",
+                name: "feedback",
+                label: "Feedback",
+                ui: {
+                  itemProps: (item) => {
+                    return { label: item?.description || "" };
+                  },
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Description",
+                    ui: {
+                      component: "textarea",
+                    },
+                  },
+                  {
+                    type: "object",
+                    name: "topics",
+                    label: "Topics",
+                    list: true,
+                    ui: {
+                      itemProps: (item) => {
+                        return { label: item?.title || "" };
+                      },
+                    },
+                    fields: [
+                      {
+                        type: "string",
+                        name: "iconName",
+                        label: "Icon Name",
+                        required: true,
+                      },
+                      {
+                        type: "string",
+                        name: "title",
+                        label: "Title",
+                        required: true,
+                      },
+                    ],
+                  },
+                ],
+              },
+              // Incentivos Object
+              {
+                type: "object",
+                name: "incentivos",
+                label: "Incentivos",
+                ui: {
+                  itemProps: (item) => {
+                    return { label: item?.description || "" };
+                  },
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "description",
+                    label: "Description",
+                    ui: {
+                      component: "textarea",
+                    },
+                  },
+                  {
+                    type: "object",
+                    name: "topics",
+                    label: "Topics",
+                    list: true,
+                    ui: {
+                      itemProps: (item) => {
+                        return { label: item?.title || "" };
+                      },
+                    },
+                    fields: [
+                      {
+                        type: "string",
+                        name: "iconName",
+                        label: "Icon Name",
+                        required: true,
+                      },
+                      {
+                        type: "string",
+                        name: "title",
+                        label: "Title",
+                        required: true,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
 });
