61e86c71abbf4c5cdd4a931356371e68af34b19a
drethesilva <andressilva03@gmail.com>
2025-07-01 18:17:11 +0100

    Not found error

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,15 +1,8 @@
-// app/not-found.tsx
-
-import Link from "next/link";
-
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-6xl font-bold text-red-500">404</h1>
       <p className="mt-4 text-lg text-gray-600">Page not found.</p>
-      <Link href={"/"} className="text-blue-600 hover:text-blue-800">
-        Go back home
-      </Link>
     </div>
   );
 }
