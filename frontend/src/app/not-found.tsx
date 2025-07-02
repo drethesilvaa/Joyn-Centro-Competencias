03815f3d9aa566e273d780896c94a28343cc2ba4
drethesilva <andressilva03@gmail.com>
2025-07-02 15:15:19 +0100

    not found page change

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,6 @@
-import { NotFoundPage } from "@/modules/not-found/page"
-
 export default function NotFound() {
-    return <NotFoundPage />
+    return <div className="flex flex-col items-center justify-center h-screen text-center">
+        <h1 className="text-6xl font-bold text-red-500">404</h1>
+        <p className="mt-4 text-lg text-gray-600">Page not found.</p>
+    </div>
 }
