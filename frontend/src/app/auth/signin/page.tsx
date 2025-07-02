e781f54c895bb5c3fe59bc4b94ef1b12f2a3c4ce
drethesilva <andressilva03@gmail.com>
2025-07-02 15:49:30 +0100

    auth siging split

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,36 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { signIn, getProviders, ClientSafeProvider, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import PageLoader from "@/layouts/PageLoader";
+import { SignInPage } from "@/modules/auth/pages";
 
 export default function SignIn() {
-    const [providers, setProviders] = useState<Record<string, ClientSafeProvider> | null>(null);
-    const { status } = useSession();
-    const router = useRouter();
-
-    useEffect(() => {
-        if (status === "authenticated") {
-            router.replace("/");
-        }
-    }, [status, router]);
-
-    useEffect(() => {
-        getProviders().then((prov) => setProviders(prov));
-    }, []);
-
-    useEffect(() => {
-        if (
-            status === "unauthenticated" &&
-            providers &&
-            providers["google"]
-        ) {
-            signIn("google");
-        }
-    }, [providers, status]);
-
-    return (
-        <PageLoader />
-    );
+    return <SignInPage />
 }
---
+++ frontend/src/modules/auth/pages/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { signIn, getProviders, ClientSafeProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import PageLoader from "@/layouts/PageLoader";
+
+
+export const SignInPage = () => {
+    const [providers, setProviders] = useState<Record<string, ClientSafeProvider> | null>(null);
+    const { status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "authenticated") {
+            router.replace("/");
+        }
+    }, [status, router]);
+
+    useEffect(() => {
+        getProviders().then((prov) => setProviders(prov));
+    }, []);
+
+    useEffect(() => {
+        if (
+            status === "unauthenticated" &&
+            providers &&
+            providers["google"]
+        ) {
+            signIn("google");
+        }
+    }, [providers, status]);
+
+    return (
+        <PageLoader />
+    );
+}
