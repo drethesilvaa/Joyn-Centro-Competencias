e67b92b21fd1e4f554c529bfcb1a5cd5a21c313d
drethesilvaa <andressilva03@gmail.com>
2025-09-22 18:55:31 +0100

    mentor cards change

--- frontend/src/modules/homepage/components/MentorCard.tsx
+++ frontend/src/modules/homepage/components/MentorCard.tsx
@@ -24,18 +24,24 @@ export default function MentorCard({
   return (
     <div
       key={k}
-      className={`perspective h-full grid justify-items-center gap-2 text-white`}
+      className={`perspective h-full grid justify-items-center gap-2 `}
     >
       <motion.div
-        className={`relative w-full h-full py-6 px-2 lg:py-4 rounded-4xl shadow-lg ${classNamesForCards[k]} cursor-pointer `}
+        className={`relative w-full h-full py-6 px-2 lg:py-4 rounded-4xl shadow-lg glass-bg glass-bg--white cursor-pointer ${
+          flipped ? "glass-bg--no-backdrop" : "glass-bg--no-backdrop"
+        } `}
         style={{ transformStyle: "preserve-3d" }}
         animate={{ rotateY: flipped ? 180 : 0 }}
         transition={{ duration: 0.6 }}
         onClick={() => setFlipped(!flipped)}
       >
         {/* Front Side */}
-        <div className={`inset-0 backface-hidden grid justify-items-center justify-center items-center gap-2 h-full hover:scale-[102%] transition-transform  `}>
-          <div className={`relative h-full w-full min-w-[134px] min-h-[134px] aspect-auto rounded-2xl overflow-hidden `}>
+        <div
+          className={`inset-0 backface-hidden grid justify-items-center justify-center items-center gap-2 h-full hover:scale-[102%] transition-transform  `}
+        >
+          <div
+            className={`relative h-full w-full min-w-[134px] min-h-[134px] aspect-auto rounded-2xl overflow-hidden `}
+          >
             <Image
               src={mentor?.image || ""}
               alt={`${mentor?.text} photo`}
@@ -49,11 +55,11 @@ export default function MentorCard({
         </div>
 
         {/* Back Side */}
-        <div className={`absolute inset-0 backface-hidden rotate-y-180 grid justify-items-center gap-2 rounded-2xl p-4`}>
+        <div
+          className={`absolute inset-0 backface-hidden rotate-y-180 grid justify-items-center gap-2 rounded-2xl p-4 glass-bg--whitter`}
+        >
           <h1 className="heading-xl font-semibold">Mais Informação</h1>
-          <p className="text-lg text-white">
-            {mentor?.subtitle || "No additional info"}
-          </p>
+          <p className="text-lg ">{mentor?.subtitle || "No additional info"}</p>
         </div>
       </motion.div>
     </div>
